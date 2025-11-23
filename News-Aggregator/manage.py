12f91a232769f1d93d76a16a51c17847f1379385
omg2804 <gaikwadom992@gmail.com>
2025-11-24 00:31:39 +0530

    fixed the image loading issue

--- News-Aggregator/manage.py
+++ News-Aggregator/manage.py
@@ -8,12 +8,14 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsAggregator.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except Exception as exc:
+        sys.stderr.write(
+            "Couldn't import Django. Make sure Django is installed in your "
+            "current Python environment (e.g. pip install django) and that "
+            "you activated the virtual environment if using one.\n"
+            f"Import error: {exc}\n"
+        )
+        sys.exit(1)
     execute_from_command_line(sys.argv)
 
 
--- News-Aggregator/news/views.py
+++ News-Aggregator/news/views.py
@@ -7,33 +7,75 @@ from news.models import Headline
 # Create your views here.
 
 
-def scrape(request, name):
-    Headline.objects.all().delete()
-    session = requests.Session()
-    session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-    url = f"https://www.theonion.com/{name}"
-    content = session.get(url).content
-    soup = BSoup(content, "html.parser")
+# def scrape(request, name):
+#     Headline.objects.all().delete()
+#     session = requests.Session()
+#     session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+#     url = f"https://www.theonion.com/{name}"
+#     content = session.get(url).content
+#     soup = BSoup(content, "html.parser")
 
-    News = soup.find_all("div", {"class": "sc-cw4lnv-13 hHSpAQ"})
+#     News = soup.find_all("div", {"class": "sc-cw4lnv-13 hHSpAQ"})
 
-    for article in News:
-        main = article.find_all("a", href=True)
+#     for article in News:
+#         main = article.find_all("a", href=True)
 
-        linkx = article.find("a", {"class": "sc-1out364-0 dPMosf js_link"})
-        link = linkx["href"]
+#         linkx = article.find("a", {"class": "sc-1out364-0 dPMosf js_link"})
+#         link = linkx["href"]
 
-        titlex = article.find("h2", {"class": "sc-759qgu-0 cvZkKd sc-cw4lnv-6 TLSoz"})
-        title = titlex.text
+#         titlex = article.find("h2", {"class": "sc-759qgu-0 cvZkKd sc-cw4lnv-6 TLSoz"})
+#         title = titlex.text
 
-        imgx = article.find("img")["data-src"]
+#         imgx = article.find("img")["data-src"]
 
-        new_headline = Headline()
-        new_headline.title = title
-        new_headline.url = link
-        new_headline.image = imgx
-        new_headline.save()
-    return redirect("../")
+#         new_headline = Headline()
+#         new_headline.title = title
+#         new_headline.url = link
+#         new_headline.image = imgx
+#         new_headline.save()
+#     return redirect("../")
+
+def scrape(request,name=None):
+    import requests
+    from bs4 import BeautifulSoup
+
+    Headline.objects.all().delete()  # optional, for clean reloads
+    
+    url = "https://www.theonion.com/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    articles = soup.find_all('article')
+
+    for a in articles:
+        title_tag = a.find('h2')
+        if not title_tag:
+            continue
+        
+        title = title_tag.get_text()
+        link = title_tag.find('a')['href']
+
+        # extract image
+        img_tag = a.find('img')
+
+        if img_tag:
+            if img_tag.get('data-src'):
+                img_url=img_tag.get('data-src')
+            elif img_tag.get('src'):
+                img_url=img_tag.get('src')
+            else:
+                img_url=""
+        else:
+            img_url=""
+        
+
+        Headline.objects.create(
+            title=title,
+            url=link,
+            image=img_url
+        )
+
+    return redirect('/')
 
 
 def news_list(request):
